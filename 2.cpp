efe4c43edbcf37764570235a9f6fe9f356c0ede7
Twixik325 <80470731+Twixik325@users.noreply.github.com>
2024-02-12 23:27:12 +0600

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void sumOfRow(int** arr, int n) {
+    int maxSum = 0;
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += arr[i][j];
+        }
+        if (maxSum < sum) {
+            maxSum = sum;
+            index = i+1;
+        }
+    }
+    cout << "Максимальная сумма элементов = " << maxSum << " под номером " << index << endl;
+}
+
+int main()
+{
+    srand(time(0));
+    setlocale(LC_ALL, "RUS");
+    int rows = 6, cols = 6;
+    int** arr = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        arr[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = rand() % 10 + 1;
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    sumOfRow(arr, 6);
+
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
