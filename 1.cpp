660c1b96b54cdabe54d36db595abcb8968862334
Twixik325 <80470731+Twixik325@users.noreply.github.com>
2024-02-12 23:26:30 +0600

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+#include <ctime>
+
+using namespace std;
+
+void func(int *arr,int n) {
+	double average;
+	int min = arr[0], max = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Минимальный элемент " << min << endl;
+	cout << "Максимальный элемент " << max << endl;
+	average = (min + max) / 2.0;
+	cout << "Среднее Арифметическое " << average << endl;
+}
+
+int main()
+{
+	srand(time(0));
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "Введите число элементов" << endl;
+	cin >> n;
+	int* arr = new int [n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 10;
+	}
+	func(arr, n);
+
+	delete[] arr;
+	return 0;
+}
